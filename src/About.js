e90f9cff8f9ec759c40a44a548ff44cd96242068
jbadan <jenna.badanowski@gmail.com>
2017-12-05 14:03:11 -0800

    added about section

--- src/About.js
+++ src/About.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import './About.css';
+import { Row, Col } from 'react-flexbox-grid';
 
 
 class About extends Component {
+
   render() {
     return (
-      <div className="questionCard">
-        What is bike packing?
-      </div>
+      <div>
+        <h3> FAQ </h3>
+        <Row>
+          <Col xs>
+            <div className="flip-container" ontouchstart="this.classList.toggle('hover');">
+              <div className="flipper">
+                <div className="front">
+                  <p>What is bikepacking?</p>
+                </div>
+                <div className="back">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+            </div>
+        </Col>
+      </Row>
+</div>
     );
   }
 }
